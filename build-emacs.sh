79a86f564fa0bd73a76d89d010989ce550bdd8db
Colin McLear <mclear@fastmail.com>
2021-12-03 12:20:15 -0500

    Clean up script

--- build-emacs.sh
+++ build-emacs.sh
@@ -10,17 +10,26 @@ ROOT_DIR="`pwd`"
 BUILD_DIR=/tmp/emacs-build
 SRC_DIR=emacs-git
 
-## Use Homebrew libxml
-export LDFLAGS="-L/opt/homebrew/opt/libxml2/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/libxml2/include"
+# ======================================================
+# Use Homebrew libxml
+# ======================================================
 
-## Start with a clean build
+# export LDFLAGS="-L/opt/homebrew/opt/libxml2/lib"
+# export CPPFLAGS="-I/opt/homebrew/opt/libxml2/include"
+
+# ======================================================
+# Start with a clean build
+# ======================================================
+
 rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
 
 cd ${SRC_DIR}
 
-## Input for version otherwise default to master
+# ======================================================
+# Input for version otherwise default to master
+# ======================================================
+
 if test -n "$1"; then
     commit="$1"
 else
@@ -35,12 +44,29 @@ TIMESTAMP=`git log -n 1 --no-color --pretty='format:%at' ${commit}`
 PATCH_LIST=`find ${ROOT_DIR}/patches/ -name '*.patch'`
 cd ${BUILD_DIR}
 
-## Apply Patches
-echo "######### Applying patches #########"
+echo "
+# ======================================================
+# Sync site Lisp & Git sha
+# ======================================================
+"
+rsync -aE ${ROOT_DIR}/site-lisp lisp
+
+sed -e "s/@@GIT_COMMIT@@/$REV/" -i '' lisp/site-lisp/early-site-start.el
+
+echo "
+# ======================================================
+# Apply Patches
+# ======================================================
+"
+# Note that this applies all patches in 'patches' dir
 for f in ${PATCH_LIST}; do
     echo "Applying patch `basename $f`"
     patch -p1 -i $f
 done
+
+# ======================================================
+# Info settings
+# ======================================================
 
 STRINGS="
   nextstep/templates/Emacs.desktop.in
@@ -54,40 +80,70 @@ VNUM=`echo $ORIG | sed 's#^AC_INIT(\(.*\))#\1#; s/ //g' | cut -f2 -d,`
 VERS="$DAY Git $REV"
 DESCR="Emacs_Cocoa_${VNUM}_${DAY}_Git_${REV}"
 
-## Autogen/copy_autogen
-echo "######### Run autogen.sh #########"
+echo "
+# ======================================================
+# Autogen/copy_autogen
+# ======================================================
+"
 ./autogen.sh
 
-## Use Homebrew libxml pkgconfig
-export PKG_CONFIG_PATH="/opt/homebrew/opt/libxml2/lib/pkgconfig"
+# ======================================================
+# Use Homebrew libxml pkgconfig
+# ======================================================
+
+# export PKG_CONFIG_PATH="/opt/homebrew/opt/libxml2/lib/pkgconfig"
 
 for f in $STRINGS; do
     sed -e "s/@version@/@version@ $VERS/" -i '' $f
 done
 
-## Configure emacs
-echo "######### Configure Emacs build flags #########"
+echo "
+# ======================================================
+# Configure emacs
+# ======================================================
+"
+
 ./configure \
+    --with-ns \
     --without-dbus \
     --with-native-compilation \
     --with-xwidgets \
     --with-mailutils \
 
 
-## Build and install everything
-echo "####### Build and install everything ########"
+echo "
+# ======================================================
+# Build and install everything
+# ======================================================
+"
+
 make
 make install
 trash /Applications/Emacs.app # trash old emacs
 mv ${BUILD_DIR}/nextstep/Emacs.app /Applications # move to applications folder
 
-echo "## Change icon"
+echo "
+# ======================================================
+# Change icon
+# ======================================================
+"
+
 cp ~/Pictures/emacs-icons/emacs-big-sur.icns /Applications/Emacs.app/Contents/Resources/Emacs.icns
 # cp ~/Pictures/emacs-icons/info.plist /Applications/Emacs.app/Contents/Info.plist
 
-echo "####### Open new emacs #######"
+echo "
+# ======================================================
+# Open new emacs
+# ======================================================
+"
+
 open /Applications/Emacs.app
 
-echo "######## Cleanup ########"
+echo "
+# ======================================================
+# Cleanup
+# ======================================================
+"
+
 rm -rf ${BUILD_DIR}
 rm -rf tmp.dmg
